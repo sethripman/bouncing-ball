0fdc0363d3d64caf34172771ead81dda312e6878
sethripman <sethripman@gmail.com>
2019-10-29 16:09:11 -0700

    multiple playing pieces on field

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 // Dom elements
 const createButton = document.getElementById('load');
 // state variables
-let myGamePiece;
-
-// start game logic
-function startGame() {
-    myGameArea.start();
-    myGamePiece = new Component(30, 30, 'red', 10, 120);
-}
-
+let myGamePiece, yellowGamePiece, blueGamePiece;
 // define myGameArea
 let myGameArea = {
     canvas : document.createElement('canvas'),
@@ -17,12 +10,21 @@ let myGameArea = {
         this.canvas.height = 540;
         this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 40);
+        this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 };
+
+// start game logic
+function startGame() {
+    myGameArea.start();
+    myGamePiece = new Component(30, 30, 'red', 10, 120);
+    yellowGamePiece = new Component(75, 75, 'yellow', 50, 60);
+    blueGamePiece = new Component(75, 75, 'blue', 10, 110);
+}
+
 
 // define component that will be placed on canvas
 function Component(width, height, color, x, y) {
@@ -40,7 +42,10 @@ function Component(width, height, color, x, y) {
 
 function updateGameArea() {
     myGameArea.clear();
+    myGamePiece.x += 1;
     myGamePiece.update();
+    yellowGamePiece.update();
+    blueGamePiece.update();
 }
 
 // add event listener to load button
